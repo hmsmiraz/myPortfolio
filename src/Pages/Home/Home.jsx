8e59aeea6b0fcc2262c39c91c33d502ac8ee86b0
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-11 13:41:13 +0600

    added navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h2 className="text-red-600 text-2xl font-bold">Home</h2>
+            <h2 className=" text-2xl font-bold">Home</h2>
         </div>
     );
 };
