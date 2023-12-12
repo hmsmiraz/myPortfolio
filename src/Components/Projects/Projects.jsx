d78914952ca4552b90f151d2ce9ca3348e0f2365
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-12 20:00:25 +0600

    done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,18 +3,47 @@ import SharedTitle from "../../Shared/SharedTitle";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    fetch("public/Projects.json")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => setProjects(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  // const [projects, setProjects] = useState([]);
+  // useEffect(() => {
+  //   fetch("public/Projects.json")
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((data) => setProjects(data))
+  //     .catch((error) => console.error("Error fetching data:", error));
+  // }, []);
+  const projects = [
+    {
+      "id": 1,
+      "name": "AssetPulse",
+      "image": "https://i.ibb.co/x87gdX8/asset.png",
+      "type": "Asset Management System",
+      "live_link": "https://assetpulse-511ac.web.app/",
+      "client_code": "https://github.com/hmsmiraz/AssetPulse",
+      "server_link": "https://github.com/hmsmiraz/AssetPulse-Server"
+    },
+    {
+      "id": 2,
+      "name": "Bytes of Thoughts",
+      "image": "https://i.ibb.co/MNhNcTD/bot.png",
+      "type": "A Blog website",
+      "live_link": "https://bytes-of-thoughts.web.app/",
+      "client_code": "https://github.com/hmsmiraz/Bytes_of_Thoughts_Client",
+      "server_link": "https://github.com/hmsmiraz/Bytes_of_Thoughts_Server"
+    },
+    {
+      "id": 3,
+      "name": "Bistro Boss",
+      "image": "https://i.ibb.co/Z1c2dR0/bistro.png",
+      "type": "Restaurant Management System",
+      "live_link": "https://bistro-boss-25724.web.app/",
+      "client_code": "https://github.com/hmsmiraz/Bistro-Boss-Client",
+      "server_link": "https://github.com/hmsmiraz/Bistro-Boss-Server"
+    }
+  ];
   return (
     <div className="my-5">
       <div>
@@ -35,15 +64,15 @@ const Projects = () => {
             <div className="card-body">
               <h2 className="card-title">{project.name}</h2>
               <p>{project.type}</p>
-              <div className="card-actions flex justify-center">
+              <div className="card-actions flex justify-center ">
                 <Link to={project.live_link}>
-                  <button className="btn btn-primary">Live Link</button>
+                  <button className="btn btn-primary text-white">Live Link</button>
                 </Link>
                 <Link to={project.client_code}>
-                  <button className="btn btn-primary">Client Code</button>
+                  <button className="btn btn-primary text-white">Client Code</button>
                 </Link>
                 <Link to={project.server_link}>
-                  <button className="btn btn-primary">Server Code</button>
+                  <button className="btn btn-primary text-white">Server Code</button>
                 </Link>
               </div>
             </div>
